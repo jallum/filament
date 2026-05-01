4b7e8cf959a5ede42670005063cfcc92723eab73
Jason Allum <21417+jallum@users.noreply.github.com>
2026-05-01 11:11:58 -0400

    flm-io2.3: ExDoc config and guides skeleton

--- filament/mix.exs
+++ filament/mix.exs
@@ -7,7 +7,9 @@ defmodule Filament.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -22,7 +24,29 @@ defmodule Filament.MixProject do
   defp deps do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.36", only: [:dev, :docs], runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Filament",
+      logo: ".tmp/logo.png",
+      extras: [
+        "guides/Getting Started.md",
+        "guides/Observables.md",
+        "guides/Resource Holds.md",
+        "guides/Migration Guide.md"
+      ]
+    ]
+  end
+
+  defp package do
+    [
+      description: "Filament is a Phoenix LiveView component library",
+      licenses: ["Apache-2.0"],
+      links: %{}
     ]
   end
 end
